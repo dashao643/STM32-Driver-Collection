160a0cc23a112bca563ce44d0eef1ba532e41b08
dashao643 <1127545598@qq.com>
2026-04-05 18:26:50 +0800

    舵机、直流电机

--- PWM_LED/pwm.c
+++ PWM_LED/pwm.c
@@ -1,6 +1,9 @@
 #include "pwm.h"
-#include "key.h"
+#include "main.h"
 #include "oled.h"
+#include "gpio.h"
+#include "stm32_hal_legacy.h"
+#include "stm32f1xx.h"
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -36,6 +39,15 @@ void PWM_Encoder_Init(void)
   HAL_TIM_Encoder_Start(PWM_ENCODER_HANDLE, TIM_CHANNEL_ALL);
 }
 
+void PWM_SERVO_Init(void)
+{
+  HAL_TIM_Base_Start(PWM_SERVO_HANDLE);
+  HAL_TIM_PWM_Start(PWM_SERVO_HANDLE, PWM_SERVO_CHANNEL);
+  PWM_SetServoAngle(90);
+}
+
+/********************* LED + 编码器 ***************** */
+
 /**
  * @brief Encoder Cnt 数值保持在0-100
  * 
@@ -69,9 +81,13 @@ static bool cntIsChange(void)
 static void setLedDuty(void)
 {
   uint8_t ccr = getEncoderCnt();
-  __HAL_TIM_SET_COMPARE(PWM_LED_HANDLE,TIM_CHANNEL_2,ccr);
+  __HAL_TIM_SET_COMPARE(PWM_LED_HANDLE,PWM_LED_CHANNEL,ccr);
 }
 
+/**
+ * @brief 检测旋转编码器的转动，设置LED灯亮度
+ * 
+ */
 void PWM_LED_Scan(void)
 {
   if(cntIsChange()){
@@ -80,4 +96,113 @@ void PWM_LED_Scan(void)
     OLED_ShowDecNumber(1, 9, getEncoderCnt(), 3);
 #endif
   }
+}
+
+/************************* 舵机 **********************/
+
+/**
+ * @brief SG90舵机，分辨率为9度
+ * 
+ * @param angle 0-180度,对应ccr 5-25
+ */
+void PWM_SetServoAngle(uint8_t angle)
+{
+  uint8_t ccrVal = angle / 9 + 5;
+  __HAL_TIM_SET_COMPARE(PWM_SERVO_HANDLE,PWM_SERVO_CHANNEL,ccrVal);
+}
+
+void PWM_AddServoAngle(void)
+{
+  uint8_t curCcr = __HAL_TIM_GET_COMPARE(PWM_SERVO_HANDLE,PWM_SERVO_CHANNEL);
+  if(curCcr < 25){
+    curCcr += SERVO_ANGLE_STEP / 9;
+    __HAL_TIM_SET_COMPARE(PWM_SERVO_HANDLE,PWM_SERVO_CHANNEL,curCcr);
+  }
+}
+
+void PWM_SubServoAngle(void)
+{
+  int8_t curCcr = __HAL_TIM_GET_COMPARE(PWM_SERVO_HANDLE,PWM_SERVO_CHANNEL);
+  if(curCcr > 5){
+    curCcr -= SERVO_ANGLE_STEP / 9;
+    __HAL_TIM_SET_COMPARE(PWM_SERVO_HANDLE,PWM_SERVO_CHANNEL,curCcr);
+  }
+#ifdef OLED
+  OLED_ShowString(2, 1, "curCcr=");
+  OLED_ShowDecNumber(2, 8, curCcr, 3);
+#endif
+}
+
+/************************ 直流电机 **********************/
+static PWM_Motor_State motor;
+
+void PWM_Motor_Init(void)
+{
+  HAL_TIM_Base_Start(PWM_MOTOR_HANDLE);
+  HAL_TIM_PWM_Start(PWM_MOTOR_HANDLE, PWM_MOTOR_CHANNEL);
+  motor.isStart = false;
+  motor.dir = forward;
+  motor.speed = 60;
+}
+
+void PWM_Motor_StartAndStop(void)
+{
+  // motorCheck();
+  if(motor.isStart){
+    HAL_GPIO_WritePin(MOTOR_CRL1_GPIO_Port,MOTOR_CRL1_Pin, RESET);
+    HAL_GPIO_WritePin(MOTOR_CRL2_GPIO_Port,MOTOR_CRL2_Pin, RESET);
+    motor.isStart = false;
+    return;
+  }
+  // 电机此时为关闭，根据设定的方向开启电机
+  if(motor.dir == forward){
+    HAL_GPIO_WritePin(MOTOR_CRL1_GPIO_Port,MOTOR_CRL1_Pin, SET);
+  }
+  else if(motor.dir == reverse){
+    HAL_GPIO_WritePin(MOTOR_CRL2_GPIO_Port,MOTOR_CRL2_Pin, SET);
+  }
+  motor.isStart = true;
+}
+
+/**
+ * @brief 电机方向反转
+ * 
+ */
+void PWM_Motor_ReverseDir(void)
+{
+  // 此时为启动状态，改变实际电平
+  if(motor.isStart){
+    HAL_GPIO_TogglePin(MOTOR_CRL1_GPIO_Port,MOTOR_CRL1_Pin);
+    HAL_GPIO_TogglePin(MOTOR_CRL2_GPIO_Port,MOTOR_CRL2_Pin);
+  }
+  if(motor.dir == forward)
+    motor.dir = reverse;
+  else
+    motor.dir = forward;
+}
+
+void PWM_Motor_SpeedAdd(void)
+{
+  if(motor.speed < 100){
+    motor.speed += MOTOR_SPEED_STEP;
+    __HAL_TIM_SetCompare(PWM_MOTOR_HANDLE, PWM_MOTOR_CHANNEL,motor.speed);
+  }
+  // LED_RED_TOGGLE();
+#ifdef OLED
+  OLED_ShowString(2, 1, "curSpeed=");
+  OLED_ShowDecNumber(2, 8, motor.speed, 3);
+#endif
+}
+
+void PWM_Motor_SpeedSub(void)
+{
+  if(motor.speed > MOTOR_SPEED_STEP){
+    motor.speed -= MOTOR_SPEED_STEP;
+    __HAL_TIM_SetCompare(PWM_MOTOR_HANDLE, PWM_MOTOR_CHANNEL,motor.speed);
+  }
+  // LED_GREEN_TOGGLE();
+#ifdef OLED
+  OLED_ShowString(2, 1, "curSpeed=");
+  OLED_ShowDecNumber(2, 8, motor.speed, 3);
+#endif
 }
--- PWM_LED/pwm.h
+++ PWM_LED/pwm.h
@@ -2,16 +2,51 @@
 #define __PWM_H__
 
 #include "tim.h"
+#include <stdint.h>
+#include <stdbool.h>
+
+#define OLED
 
 #define PWM_LED_HANDLE        &htim2
 #define PWM_LED_CHANNEL       TIM_CHANNEL_2
 
 #define PWM_ENCODER_HANDLE    &htim1
 
-#define OLED
+#define PWM_SERVO_HANDLE      &htim3
+#define PWM_SERVO_CHANNEL     TIM_CHANNEL_1
+
+#define PWM_MOTOR_HANDLE      &htim2
+#define PWM_MOTOR_CHANNEL     TIM_CHANNEL_1
+
+#define SERVO_ANGLE_STEP      45
+#define MOTOR_SPEED_STEP      20
 
 void PWM_LED_Init(void);
 void PWM_Encoder_Init(void);
+void PWM_SERVO_Init(void);
+
 void PWM_LED_Scan(void);
+
+void PWM_SetServoAngle(uint8_t angle);
+void PWM_AddServoAngle(void);
+void PWM_SubServoAngle(void);
+
+/************************ 直流电机 **********************/
+typedef enum{
+  forward = 1,
+  reverse = 0
+} PWM_Motor_Dir;
+
+typedef struct{
+  bool isStart;
+  PWM_Motor_Dir dir;
+  uint8_t speed;
+} PWM_Motor_State;
+
+void PWM_Motor_Init(void);
+void PWM_Motor_StartAndStop(void);
+void PWM_Motor_ReverseDir(void);
+void PWM_Motor_SpeedAdd(void);
+void PWM_Motor_SpeedSub(void);
 
 #endif
