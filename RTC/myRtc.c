ac65279d2dc659549ab31ae6483eca0d94f3b4e7
dashao643 <1127545598@qq.com>
2026-04-12 22:53:55 +0800

    RTC实现实时显示日期、时间，定时闹钟调用回调

--- RTC/myRtc.c
+++ RTC/myRtc.c
@@ -3,6 +3,7 @@
 
 #include "stdio.h"
 #include "oled.h"
+#include "gpio.h"
 
 static bool showFlag = false;
 
@@ -10,24 +11,63 @@ static RTC_TimeTypeDef rtcTime = {0};
 static RTC_DateTypeDef rtcDate = {0};
 
 /**
+ * @brief 如果校准过时间，把日期从BKP中读出来，同步日期
+ * 
+ */
+static void restoreDateFromBKP(void)
+{
+  rtcDate.Year = HAL_RTCEx_BKUPRead(&hrtc, BKUP_REG_YEAR);
+  rtcDate.Month = HAL_RTCEx_BKUPRead(&hrtc, BKUP_REG_MONTH);
+  rtcDate.Date = HAL_RTCEx_BKUPRead(&hrtc, BKUP_REG_DATE);
+  HAL_RTC_SetDate(&hrtc, &rtcDate, RTC_FORMAT_BIN);
+}
+
+/**
+ * @brief 校准时间时，把日期存入BKP
+ * 
+ * @param rtcDate 日期结构体
+ */
+static void writeDateToBKP(RTC_DateTypeDef *rtcDate)
+{
+  HAL_RTCEx_BKUPWrite(&hrtc, BKUP_REG_YEAR, rtcDate->Year);
+  HAL_RTCEx_BKUPWrite(&hrtc, BKUP_REG_MONTH, rtcDate->Month);
+  HAL_RTCEx_BKUPWrite(&hrtc, BKUP_REG_DATE, rtcDate->Date);
+}
+
+static void setCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate)
+{
+  HAL_RTC_SetTime(&hrtc, sTime, RTC_FORMAT_BIN);
+  HAL_RTC_SetDate(&hrtc, sDate, RTC_FORMAT_BIN);
+}
+
+/**
  * @brief 检查BKP寄存器1（16位）是否已置标志
  * 
  * @return true 已置位
  * @return false 未置位
  */
-bool RTC_Check_BKUP_REG1(void)
+bool RTC_CheckBkpReg1(void)
 {
-  if(HAL_RTCEx_BKUPRead(&hrtc, RTC_BKP_DR1) == BKUP_REG1_FLAG)
+  if(HAL_RTCEx_BKUPRead(&hrtc, BKUP_REG_FLAG) == BKUP_FLAG_MAGIC){
+    restoreDateFromBKP();
     return true;
-  HAL_RTCEx_BKUPWrite(&hrtc, RTC_BKP_DR1, BKUP_REG1_FLAG);
-
+  }
   return false;
 }
 
-void RTC_SetCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate)
+/**
+ * @brief 每小时触发一次
+ * 
+ */
+void RTC_SetAlarm(void)
 {
-  HAL_RTC_SetTime(&hrtc, sTime, RTC_FORMAT_BIN);
-  HAL_RTC_SetDate(&hrtc, sDate, RTC_FORMAT_BIN);
+  RTC_GetCurTimeDate(&rtcTime,&rtcDate);
+  RTC_AlarmTypeDef alarm = {0};
+  alarm.Alarm = RTC_ALARM_A;
+  alarm.AlarmTime.Hours = rtcTime.Hours + 1;
+  alarm.AlarmTime.Minutes = rtcTime.Minutes;
+  alarm.AlarmTime.Seconds = 0;
+  HAL_RTC_SetAlarm_IT(&hrtc, &alarm, RTC_FORMAT_BIN);
 }
 
 void RTC_GetCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate)
@@ -37,17 +77,18 @@ void RTC_GetCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate)
 }
 
 /***********************************************/
-// 先用手动校准
-void timeDateAdjust(void)
+// 先用手动校准，校准后置标志，再把年月日存入BKP
+void RTC_TimeDateAdjust(void)
 {
-  rtcTime.Hours = 23;
-  rtcTime.Minutes = 59;
-  rtcTime.Seconds = 50;
+  rtcTime.Hours = 22;
+  rtcTime.Minutes = 52;
+  rtcTime.Seconds = 0;
   rtcDate.Year = 26;
   rtcDate.Month = 4;
-  rtcDate.Date = 11;
-  rtcDate.WeekDay = 6;
-  RTC_SetCurTimeDate(&rtcTime, &rtcDate);
+  rtcDate.Date = 12;
+  setCurTimeDate(&rtcTime, &rtcDate);
+  HAL_RTCEx_BKUPWrite(&hrtc, BKUP_REG_FLAG, BKUP_FLAG_MAGIC);
+  writeDateToBKP(&rtcDate);
 }
 
 void RTC_SetShowFlag(void)
@@ -55,9 +96,13 @@ void RTC_SetShowFlag(void)
   showFlag = true;
 }
 
+/**
+ * @brief 主循环调用
+ * 
+ */
 void RTC_OLED_ShowTask(void)
 {
-  // if(showFlag){
+  if(showFlag){
     showFlag = false;
     RTC_GetCurTimeDate(&rtcTime, &rtcDate);
 
@@ -67,7 +112,19 @@ void RTC_OLED_ShowTask(void)
     snprintf(dateBuf, sizeof(dateBuf), "%04d-%02d-%02d", 2000 + rtcDate.Year, rtcDate.Month,rtcDate.Date);
     snprintf(timeBuf, sizeof(timeBuf), "%02d:%02d:%02d", rtcTime.Hours, rtcTime.Minutes,rtcTime.Seconds);
 
-    OLED_ShowString(1, 1, dateBuf);
-    OLED_ShowString(2, 1, timeBuf);
-  // }
+    OLED_ShowString(3, 1, dateBuf);
+    OLED_ShowString(4, 1, timeBuf);
+  }
+}
+
+void RTC_ClearRegFlag(void)
+{
+  HAL_RTCEx_BKUPWrite(&hrtc, BKUP_REG_FLAG, 0);
+}
+
+void HAL_RTC_AlarmAEventCallback(RTC_HandleTypeDef *hrtc)
+{
+  RTC_GetCurTimeDate(&rtcTime, &rtcDate);
+  printf("time=%d:%d:%d\n",rtcTime.Hours,rtcTime.Minutes,rtcTime.Seconds);
+  RTC_SetAlarm();
 }
--- RTC/myRtc.h
+++ RTC/myRtc.h
@@ -4,15 +4,22 @@
 #include "stm32f1xx_hal.h"
 #include <stdbool.h>
 
-#define BKUP_REG1_FLAG  0xAA55
+#define BKUP_FLAG_MAGIC     0xAA55
 
-bool RTC_Check_BKUP_REG1(void);
-void RTC_SetCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate);
+#define BKUP_REG_FLAG     RTC_BKP_DR1
+#define BKUP_REG_YEAR     RTC_BKP_DR2
+#define BKUP_REG_MONTH    RTC_BKP_DR3
+#define BKUP_REG_DATE     RTC_BKP_DR4
+
+bool RTC_CheckBkpReg1(void);
+void RTC_SetAlarm(void);
 void RTC_GetCurTimeDate(RTC_TimeTypeDef *sTime, RTC_DateTypeDef *sDate);
 
-void timeDateAdjust(void);
+// RTC全局中断和Alarm中断同时开启，会死锁，问题待解决
+void RTC_TimeDateAdjust(void);
 void RTC_SetShowFlag(void);
 void RTC_OLED_ShowTask(void);
+void RTC_ClearRegFlag(void);
 
 #endif
 
