bd7a279f097f9a27e2744fdfecfd772faa7c37c5
dashao643 <1127545598@qq.com>
2026-04-20 23:37:54 +0800

    状态机实现上电初始化配置。状态机简化，收发数据帧待实现

--- Esp8266/esp8266.c
+++ Esp8266/esp8266.c
@@ -1,64 +1,208 @@
 #include "esp8266.h"
 
-#include "gpio.h"
-#include "stdio.h"
-#include <stdint.h>
 #include "modbus.h"
+#include "gpio.h"
+#include "stm32f1xx_hal.h"
 
-static UART_ChannelTypeDef esp8266;
+#include <stdio.h>
+#include <string.h>
+
+
 static uint8_t rx_buf[ESP8266_RX_BUFF_MAXLENTH];
+static UART_ChannelTypeDef esp8266_uart;
+static ESP8266_HandleTypeDef esp8266_handle;
 
-static uint8_t esp8266_tx_buff[ESP8266_TX_BUFF_MAXLENTH];
-static uint8_t esp8266_tx_index = 0;
+// static uint8_t esp8266_tx_buff[ESP8266_TX_BUFF_MAXLENTH];
+// static uint8_t esp8266_tx_index = 0;
 
-static void frameReply(void)
+static void stateClear(void)
 {
-  HAL_UART_Transmit(ESP8266_UARTX, esp8266_tx_buff, esp8266_tx_index, ESP8266_UARTX_TIMEOUT);
-  esp8266_tx_index = 0;
+  esp8266_handle.curRetryCnt = 0;
+  esp8266_handle.replyFlag = NONE;
+  esp8266_handle.timeTick = HAL_GetTick();
 }
-    // // 只有缓冲区里包含OK/ERROR，才是ESP真实回复，才打印
-    // if(strstr((char*)esp8266.buffer, "OK") != NULL || 
-    //    strstr((char*)esp8266.buffer, "ERROR") != NULL)
-    // {
-    //     printf("%s\n",esp8266.buffer);
-    // }
-    // // 自己串扰回来的AT指令，直接丢弃不打印
+
+static void AT_Transmit(char cmd[])
+{
+  uint16_t len = strlen(cmd);
+  HAL_UART_Transmit(ESP8266_UARTX, (uint8_t*)cmd, len, ESP8266_UARTX_TIMEOUT);
+}
+
+static void AT_SetReplyFlag(void)
+{
+  if(strstr((char*)esp8266_uart.buffer, "OK")){
+    esp8266_handle.replyFlag = AT_OK;
+    return;
+  }
+
+  if(strstr((char*)esp8266_uart.buffer, "ERROR")){
+    esp8266_handle.replyFlag = AT_ERROR;
+    return;
+  }
+
+  if(strstr((char*)esp8266_uart.buffer, "ready")){
+    esp8266_handle.replyFlag = AT_READY;
+    return;
+  }
+}
+
+static void AT_Config(void)
+{
+  // printf("%d ",configState);
+  switch (esp8266_handle.state){
+  case INITIAL:{
+    if((HAL_GetTick() - esp8266_handle.timeTick) > INITIAL_DELAY_MS){
+      esp8266_handle.timeTick = HAL_GetTick();
+      esp8266_handle.state = SEND_AT;
+    }
+    break;
+  }
+  case SEND_AT:{
+    if(esp8266_handle.curRetryCnt == RETRY_COUNT){
+      esp8266_handle.state = FAIL;
+      return;
+    }
+    esp8266_handle.curRetryCnt++;
+    AT_Transmit("AT\r\n");
+    esp8266_handle.state = WAIT_AT_OK;
+    esp8266_handle.timeTick = HAL_GetTick();
+    break;
+  }
+  case WAIT_AT_OK:{
+    if(esp8266_handle.replyFlag == AT_OK){
+      esp8266_handle.state = SEND_RST;
+      stateClear();
+    }
+    if((HAL_GetTick() - esp8266_handle.timeTick) > WAIT_AT_REPLY_MS){
+      esp8266_handle.state = SEND_AT;
+      esp8266_handle.timeTick = HAL_GetTick();
+    }
+    break;
+  }
+  // 重启只发一次
+  case SEND_RST:{
+    AT_Transmit("AT+RST\r\n");
+    esp8266_handle.timeTick = HAL_GetTick();
+    esp8266_handle.state = WAIT_RST;
+    break;
+  }
+  case WAIT_RST:{
+    if((HAL_GetTick() - esp8266_handle.timeTick) > WAIT_RST_DELAY){
+      esp8266_handle.state = SET_CIPMUX;
+      esp8266_handle.timeTick = HAL_GetTick();
+    }
+    break;
+  }
+  case SET_CIPMUX:{
+    if(esp8266_handle.curRetryCnt == RETRY_COUNT){
+      esp8266_handle.state = FAIL;
+      return;
+    }
+    esp8266_handle.curRetryCnt++;
+    AT_Transmit("AT+CIPMUX=1\r\n");
+    esp8266_handle.state = WAIT_CIPMUX;
+    esp8266_handle.timeTick = HAL_GetTick();
+    break;
+  }
+  case WAIT_CIPMUX:{
+    if(esp8266_handle.replyFlag == AT_OK){
+      esp8266_handle.state = START_SERVICE;
+      stateClear();
+    }
+    if((HAL_GetTick() - esp8266_handle.timeTick) > WAIT_AT_REPLY_MS){
+      esp8266_handle.state = SET_CIPMUX;
+      esp8266_handle.timeTick = HAL_GetTick();
+    }
+    break;
+  }
+  case START_SERVICE:{
+    if(esp8266_handle.curRetryCnt == RETRY_COUNT){
+      esp8266_handle.state = FAIL;
+      return;
+    }
+    esp8266_handle.curRetryCnt++;
+    AT_Transmit("AT+CIPSERVER=1,80\r\n");
+    // AT_Transmit("AT+DASHAO=1,80\r\n");
+    esp8266_handle.state = WAIT_SERVICE;
+    esp8266_handle.timeTick = HAL_GetTick();
+    break;
+  }
+  case WAIT_SERVICE:{
+    if(esp8266_handle.replyFlag == AT_OK){
+      esp8266_handle.state = FINISH;
+      stateClear();
+    }
+    if((HAL_GetTick() - esp8266_handle.timeTick) > WAIT_AT_REPLY_MS){
+      esp8266_handle.state = START_SERVICE;
+      esp8266_handle.timeTick = HAL_GetTick();
+    }
+    break;
+  }
+  case FINISH:{
+    LED_GREEN_TOGGLE();
+    esp8266_handle.setInfoFlag = true;
+    break;
+  }
+  case FAIL:{
+    LED_RED_TOGGLE();
+    esp8266_handle.setInfoFlag = true;
+    break;
+  }
+  default:
+    break;
+  }
+}
+
 static void frameProcess(void)
 {
-  // 数据回显,通过串口1发送
-  printf("%s\n",esp8266.buffer);
-  // HAL_UART_Transmit(MODBUS_UARTX, esp8266.buffer, esp8266.index, ESP8266_UARTX_TIMEOUT);
+  // 设置重启后不解析数据
+  if(esp8266_handle.state == WAIT_RST) return;
+
+  printf("%s\n",esp8266_uart.buffer);
+
+  if(!esp8266_handle.setInfoFlag) {
+    AT_SetReplyFlag();
+  }
 }
 
+// .c文件里定义的数组，传入结构体，使用结构体里的数组
 void ESP8266_Init(void)
 {
-  UART_Channel_Init(&esp8266,rx_buf,ESP8266_RX_BUFF_MAXLENTH);
-  __HAL_UART_ENABLE_IT(ESP8266_UARTX,UART_IT_IDLE);
-  HAL_UART_Receive_IT(ESP8266_UARTX, esp8266.buffer, 1);
+  UART_Channel_Init(&esp8266_uart,rx_buf,ESP8266_RX_BUFF_MAXLENTH);
+  __HAL_UART_ENABLE_IT(ESP8266_UARTX, UART_IT_IDLE);
+  HAL_UART_Receive_IT(ESP8266_UARTX, esp8266_uart.buffer, 1);
+  esp8266_handle.state = INITIAL;
+  esp8266_handle.replyFlag = NONE;
+  esp8266_handle.curRetryCnt = 0;
+  esp8266_handle.timeTick = HAL_GetTick();
+  esp8266_handle.setInfoFlag = false;
 }
 
 void ESP8266_SingleByteProcess(void)
 {
-  UART_Channel_SingleByteProcess(&esp8266, ESP8266_UARTX);
+  UART_Channel_SingleByteProcess(&esp8266_uart, ESP8266_UARTX);
 }
 
 void ESP8266_SetFrameEndFlag(void)
 {
-  UART_Channel_SetFrameEndFlag(&esp8266);
+  UART_Channel_SetFrameEndFlag(&esp8266_uart);
 }
 
 void ESP8266_Task(void)
 {
-  if(esp8266.frameEnd){
+  if(!esp8266_handle.setInfoFlag){
+    AT_Config();
+  }
+  if(esp8266_uart.frameEnd){
     frameProcess();
-    LED_RED_TOGGLE();
     HAL_UART_AbortReceive_IT(ESP8266_UARTX);
-    UART_Channel_Clear(&esp8266);
-    HAL_UART_Receive_IT(ESP8266_UARTX, esp8266.buffer, 1);
+    UART_Channel_Clear(&esp8266_uart);
+    HAL_UART_Receive_IT(ESP8266_UARTX, esp8266_uart.buffer, 1);
   }
 }
 
-void ESP8266_AT_Transmit(char cmd[],uint16_t len)
+void ESP8266_AT_Transmit(char cmd[])
 {
+  uint16_t len = strlen(cmd);
   HAL_UART_Transmit(ESP8266_UARTX, (uint8_t*)cmd, len, ESP8266_UARTX_TIMEOUT);
 }
--- Esp8266/esp8266.h
+++ Esp8266/esp8266.h
@@ -4,7 +4,9 @@
 #include "my_uart.h"
 #include "stm32f1xx.h"
 #include "stm32f1xx_hal.h"
+
 #include <stdint.h>
+#include <stdbool.h>
 
 #define ESP8266_INSTANCE                 USART2
 #define ESP8266_UARTX                    &huart2
@@ -12,13 +14,59 @@
 #define ESP8266_RX_BUFF_MAXLENTH         100      // 最大帧长度
 #define ESP8266_RX_BUFF_MINLENTH         10       // 最小帧长度
 
-#define ESP8266_TX_BUFF_MAXLENTH         64      // 回复帧最大帧长
+// #define ESP8266_TX_BUFF_MAXLENTH         64       // 回复帧最大帧长
+
+#define INITIAL_DELAY_MS                 500      // 上电延时发送
+#define WAIT_RST_DELAY                   800      // 发完RST间隔时间
+#define WAIT_AT_REPLY_MS                 500      // 重试间隔
+#define RETRY_COUNT                      5        // 重试次数
+
+// 指令表字符串数组
+const char AT_CmdList[4][]{
+  {"AT\r\n"},
+  {"AT+RST\r\n"},
+  {"AT+CIPMUX=1\r\n"},
+  {"AT+CIPSERVER=1,80\r\n"}
+};
+
+typedef enum{
+  NONE = 0,
+  AT_OK,
+  AT_ERROR,
+  AT_READY,
+}ESP8266_AT_ReplyFrame;
+
+typedef enum{
+  INITIAL = 0,
+  SEND_AT,
+  WAIT_AT_OK,
+  SEND_RST,
+  WAIT_RST,
+  SET_MODE,
+  WAIT_MODE,
+  SET_WiFi_CONFIG,
+  WAIT_WiFi,
+  SET_CIPMUX,
+  WAIT_CIPMUX,
+  START_SERVICE,
+  WAIT_SERVICE,
+  FINISH,
+  FAIL,
+}ESP8266_ConfigState;
+
+typedef struct {
+  ESP8266_ConfigState state;            // 当前状态
+  ESP8266_AT_ReplyFrame replyFlag;      // 收到回复帧标志
+  uint8_t curRetryCnt;                  // 当前重传次数
+  uint32_t timeTick;                    // 定时器计数
+  bool setInfoFlag;                     // 配置完成后发送一次信息
+}ESP8266_HandleTypeDef;
 
 void ESP8266_Init(void);
 void ESP8266_SingleByteProcess(void);
 void ESP8266_SetFrameEndFlag(void);
 void ESP8266_Task(void);
 
-void ESP8266_AT_Transmit(char cmd[],uint16_t len);
+void ESP8266_AT_Transmit(char cmd[]);
 
 #endif
