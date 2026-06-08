78b292e08dead7cfb81a31d534dcbbfdd232df6a
dashao643 <1127545598@qq.com>
2026-06-08 23:15:04 +0800

    独立看门狗验证已无问题，窗口看门狗喂狗函数还是有问题

--- xWDG/xwdg.c
+++ xWDG/xwdg.c
@@ -1,12 +1,15 @@
 #include "stm32f1xx_hal.h"
+#include "stm32f1xx_hal_wwdg.h"
 #include "xwdg.h"
 
-static uint32_t iwdgTimer = 0;
+#include <stdint.h>
+#include <stdio.h>
 
-/**
- * @brief 初始化喂狗间隔定时器，在初始化函数最后调用
- * 
- */
+static uint32_t iwdgTimer = 0;
+static uint32_t wwdgTimer = 0;
+/********************* IWDG *********************/
+
+// 初始化独立看门狗定时器，在用户初始化函数最后调用
 void IWDG_Init(void)
 {
   // 当前间隔：(64 * 1875) / 40000 = 3s
@@ -20,10 +23,49 @@ void IWDG_Init(void)
   iwdgTimer = HAL_GetTick();
 }
 
+/**
+ * @brief 喂狗函数
+ * 
+ * @param intervalMs 喂狗间隔(单位：ms)
+ */
 void IWDG_Refresh(uint16_t intervalMs)
 {
   if((HAL_GetTick() - iwdgTimer) > intervalMs){
     iwdgTimer = HAL_GetTick();
     HAL_IWDG_Refresh(&hiwdg);
   } 
+}
+
+/********************* WWDG *********************/
+void WWDG_Init(void)
+{
+  // 需要在Counter的值在Window值与0x40(64)之间刷新，0x40(64)->0x3F(63)时产生复位
+  // 当前最短间隔：(4096 * 8) * (127 - 100 + 1) / 36MHz = 25.46ms 28
+  // 当前最长间隔：(4096 * 8) * (127 - 64 + 1) / 36MHz = 58.25ms  64
+  hwwdg.Instance = WWDG;
+  hwwdg.Init.Prescaler = WWDG_PRESCALER_8;
+  hwwdg.Init.Window = 127;
+  hwwdg.Init.Counter = 127;
+  hwwdg.Init.EWIMode = WWDG_EWI_DISABLE;
+
+  HAL_WWDG_Init(&hwwdg);
+
+  wwdgTimer = HAL_GetTick();
+}
+
+void WWDG_Refresh(uint16_t intervalMs)
+{
+  if((HAL_GetTick() - wwdgTimer) > intervalMs){
+    wwdgTimer = HAL_GetTick();
+    // printf("dashao\n");
+    uint8_t state = HAL_WWDG_Refresh(&hwwdg);
+    // printf("state=%d\n",state);
+  } 
+  
+}
+
+// EWI中断回调函数，在复位的前一个时钟触发
+void HAL_WWDG_EarlyWakeupCallback(WWDG_HandleTypeDef *hwwdg)
+{
+
 }
--- xWDG/xwdg.h
+++ xWDG/xwdg.h
@@ -5,20 +5,20 @@
 #include "wwdg.h"
 #include <stdint.h>
 
-// 看门狗调试会复位
+// 看门狗断点调试时也会复位
 
 /********************* IWDG *********************/
-// 喂狗最大间隔秒数计算公式：PSC * (Reload + 1) / 40000(LSI)
+// 喂狗间隔秒数计算公式：PSC * (Reload + 1) / 40KHz(LSI)
 // 喂狗最大间隔：(256 * 4096) / 40000(LSI) = 26.2s
 // 喂狗最小间隔：(4 * 1) / 40000(LSI) = 0.1ms
-
-/********************* IWDG *********************/
 void IWDG_Init(void);
 void IWDG_Refresh(uint16_t intervalMs);
 
 /********************* WWDG *********************/
-
-
-/********************* WWDG *********************/
+// 喂狗间隔秒数计算公式：4096 * PSC * (CNT - WIN + 1) / 36MHz(APB1)
+// 喂狗最大间隔(36MHz)：(4096 * 8) * (127 - 64 + 1) / 36MHz = 58.25ms
+// 喂狗最小间隔(36MHz)：(4096 * 1) * (x - x + 1) / 36MHz = 113us
+void WWDG_Init(void);
+void WWDG_Refresh(uint16_t intervalMs);
 
 #endif
