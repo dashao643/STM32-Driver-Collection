9704a2586fac79861aeca6954df638f2c766c9e3
dashao643 <1127545598@qq.com>
2026-07-12 10:27:49 +0800

    新增上位机指令: 传输数据写入w25q64

--- App/modbus_app.c
+++ App/modbus_app.c
@@ -1,7 +1,16 @@
 #include "modbus_app.h"
 #include "modbus.h"
+#include "stm32f1xx_hal.h"
 
+#include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
+
+#define LED
+// #define DHT11
+#define MY_RTC
+// #define ESP8266
+#define W25Q64
 
 #ifdef LED
 #include "led.h"
@@ -19,13 +28,16 @@
 #include "esp8266.h"
 #endif
 
-
+#ifdef W25Q64
+#include "w25q64.h"
+#endif
 
 // 检验寄存器地址
 bool Modbus_App_Check_Address(uint8_t func, uint16_t addr)
 {
-  if((addr == 0 )|| (addr > REG_ADDR_MAX)) return false;
-  if(func == MODBUS_FUNC_IAP_HANDSHAKE && addr != 0x01) return false;
+  // if((addr == 0 )|| (addr > REG_ADDR_MAX)) return false;
+  if(func == MODBUS_FUNC_W25Q64_WRITE && addr >= W25Q64_PAGE_CNT) return false;
+  else if(func == MODBUS_FUNC_IAP_HANDSHAKE && addr != 0x01) return false;
 
   return true;
 }
@@ -33,9 +45,10 @@ bool Modbus_App_Check_Address(uint8_t func, uint16_t addr)
 // 校验寄存器数量
 bool Modbus_App_Check_RegCount(uint8_t func, uint16_t cnt)
 {
-  if((cnt == 0) || (cnt > REG_CNT_MAX)) return false;
+  // if((cnt == 0) || (cnt > REG_CNT_MAX)) return false;
   if(func == MODBUS_FUNC_WRITE_SINGLE_COIL && cnt != 1) return false;
   else if(func == MODBUS_FUNC_IAP_HANDSHAKE && cnt != 1) return false;
+  else if(func == MODBUS_FUNC_W25Q64_WRITE && cnt >= W25Q64_PAGE_CNT) return false;
 
   return true;
 }
@@ -73,16 +86,16 @@ void Modbus_App_Write_Coil(uint16_t addr, uint8_t value)
     else if(value == MODBUS_SET) LED_RED_ON();
     else LED_RED_TOGGLE();
   }
-  if(addr == LED_GREEN){
-    if(value == MODBUS_RESET) LED_GREEN_OFF();
-    else if(value == MODBUS_SET) LED_GREEN_ON();
-    else LED_GREEN_TOGGLE();
-  }
-  if(addr == LED_BLUE){
-    if(value == MODBUS_RESET) LED_BLUE_OFF();
-    else if(value == MODBUS_SET) LED_BLUE_ON();
-    else LED_BLUE_TOGGLE();
-  }
+  // if(addr == LED_GREEN){
+  //   if(value == MODBUS_RESET) LED_GREEN_OFF();
+  //   else if(value == MODBUS_SET) LED_GREEN_ON();
+  //   else LED_GREEN_TOGGLE();
+  // }
+  // if(addr == LED_BLUE){
+  //   if(value == MODBUS_RESET) LED_BLUE_OFF();
+  //   else if(value == MODBUS_SET) LED_BLUE_ON();
+  //   else LED_BLUE_TOGGLE();
+  // }
 #endif
 #ifdef ESP8266
   if(addr == ESP8266_A){
@@ -96,7 +109,7 @@ bool Modbus_App_Write_Reg(uint16_t addr, const uint8_t value[])
 {
   // 设置日期和时间，地址从年开始
 #ifdef MY_RTC
-  if((addr != RTC_DATE_YEAR) || (value[0] != 6))
+  if((addr != RTC_DATE_TIME) || (value[0] != 6))
     return false;
   RTC_TimeTypeDef time = {0};
   RTC_DateTypeDef date = {0};
@@ -106,8 +119,64 @@ bool Modbus_App_Write_Reg(uint16_t addr, const uint8_t value[])
   time.Hours = value[4];
   time.Minutes = value[5];
   time.Seconds = value[6];
-  RTC_TimeDateAdjust(&time, &date);
+  RTC_SetDateTime(&date, &time);
+  return true;
 #endif
+
+  return false;
+}
+
+/**
+ * @brief 阻塞等待空闲中断标志位
+ * @return true 等待超时
+ */
+static bool waitFrameTimeout(void)
+{
+  uint8_t timer = 0;
+
+  while(timer < W25Q64_WAIT_TIMEOUT) {
+    if(Modbus_Get_UART()->frameEnd) {
+      Modbus_Get_UART()->frameEnd = false;
+      return false;
+    }
+    HAL_Delay(1);
+    timer++;
+  }
+  return true;
+}
+
+// basePage: 0-65535
+bool Modbus_App_W25Q64(uint16_t basePage, uint16_t pageCnt)
+{
+  uint8_t ack = W25Q64_ACK;
+  uint8_t curTimes = 0;
+
+  int16_t sectorIdxLast = -1;
+  while(curTimes < pageCnt) {
+    Modbus_Transmit(&ack, 1);
+    // 清除帧结束标志, 准备接收数据
+    Modbus_Get_UART()->frameEnd = false;
+
+    if(waitFrameTimeout()) return false;
+    // printf("size=%d\n", Modbus_Get_UART()->rxSize);
+    if(Modbus_Get_UART()->rxSize != 256) return false;
+
+    // 第一次跨页写的时候, 先执行页擦除
+    uint16_t sectorIdx = basePage / 16;
+    if(sectorIdxLast != sectorIdx) {
+      W25Q64_EraseSector(sectorIdx);
+      sectorIdxLast = sectorIdx;
+      // printf("exec erase\n");
+    }
+    W25Q64_WritePage(basePage, Modbus_Get_UART()->rxBuf, 256);
+    basePage++;
+    curTimes++;
+  }
+  Modbus_Transmit(&ack, 1);
+
+  // 通知状态机继续推进到 REPLY → RESET
+  Modbus_Get_UART()->frameEnd = true;
+
   return true;
 }
 
